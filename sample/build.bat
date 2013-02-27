9db7c78a62b2c89abed6280857ced8d818460a9a
Vijay Venkatachalam <vijay.venkatachalam@citrix.com>
2013-02-27 12:03:22 +0530

    Removed src/com javadoc & sample

--- sample/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-%JDK_HOME%/jdk1.5/bin/javac -cp ../lib/nitro.jar;../lib/gson.jar set_config.java
-%JDK_HOME%/jdk1.5/bin/javac -cp ../lib/nitro.jar;../lib/gson.jar get_config.java
-%JDK_HOME%/jdk1.5/bin/javac -cp ../lib/nitro.jar;../lib/gson.jar stats.java
-%JDK_HOME%/jdk1.5/bin/javac -cp ../lib/nitro.jar;../lib/gson.jar rm_config.java
-%JDK_HOME%/jdk1.5/bin/javac -cp ../lib/nitro.jar;../lib/gson.jar MyFirstNitroApplication.java
